eba3017972eb9b4550bfa51b3d4adf05816c679c
mustapha <moustaphaoussama1818@gmail.com>
2024-11-09 17:58:52 +0100

    Feat : Create a func that displays each book in own card

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const myLibrary = [];
 
+
+
+showBooks();
+
+
+
 function Book(title, author, numberPages, haveRead) {
     this.title = title;
     this.author = author;
@@ -19,8 +25,40 @@ function addBookToLibrary( title, author, numberPages, haveRead ) {
 
 // displays books
 function showBooks() {
+    const booksContainer = document.querySelector('.books-container');
     myLibrary.forEach( book => {
-        console.log(book);
+        booksContainer.appendChild(
+            createBookCard(book)
+        );
     })
 }
 
+function createBookCard( book ) {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card'); 
+
+        const titleElement = createParagraphElement( 'title' );
+        titleElement.textContent = book.title ;
+        cardDiv.appendChild( titleElement );
+
+        const authorElement = createParagraphElement( 'author' );
+        authorElement.textContent = `Author : ${book.author}` ;
+        cardDiv.appendChild( authorElement );
+
+        const numberPagesElement = createParagraphElement( 'numberPages' );
+        numberPagesElement.textContent = `Pages : ${book.numberPages}` ;
+        cardDiv.appendChild( numberPagesElement );
+
+        const haveReadElement = createParagraphElement( 'haveRead' );
+        haveReadElement.textContent = `Status :${book.haveRead}` ;
+        cardDiv.appendChild( haveReadElement );
+
+
+    return cardDiv;
+}
+
+function createParagraphElement( dataName ) {
+    const p = document.createElement('p');
+    p.classList.add(`book-${dataName}`);
+    return p;
+}
